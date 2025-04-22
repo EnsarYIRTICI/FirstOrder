eaeac7fd70f4c6974bdee9d2465cf631ef334efc
EnsarYIRTICI <ensaryrtc@hotmail.com>
2025-04-22 20:34:55 +0300

    app

--- Profile/App.ps1
+++ Profile/App.ps1
@@ -1,7 +1,22 @@
-function First-Order {
+function FirstOrder {
+    param(
+        [string]$Code
+    )
+
     Ensure-TerminalReady
 
+    $appPath = "C:\Users\ensar\repo\powershell\FirstOrder"
     $scriptPath = "C:\Users\ensar\repo\powershell\FirstOrder\Main.ps1"
+
+    if($Code -eq "vs") {
+        code $appPath
+        return
+    }
+
+    if($Code -eq "t") {
+        cd $appPath
+        return
+    } 
 
     if (-not (Test-Path $scriptPath)) {
         Write-Error "Main.ps1 bulunamadı: $scriptPath"
