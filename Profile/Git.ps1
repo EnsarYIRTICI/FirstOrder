fa5e6661a0be797ccee0893001ea000747b5c59b
EnsarYIRTICI <ensaryrtc@hotmail.com>
2025-10-10 21:53:17 +0300

    feat: gitfsc

--- Profile/Git.ps1
+++ Profile/Git.ps1
@@ -8,6 +8,22 @@ function gitfs {
     git push origin master
 }
 
+function gitfsc {
+    param(
+        [string]$M
+    )
+
+    if (-not $M) {
+        $M = Read-Host "Commit mesajını gir"
+    }
+    if ([string]::IsNullOrWhiteSpace($M)) {
+        $M = "fast"
+    }
+
+    git add .
+    git commit -m $M
+    git push origin master
+}
 
 function Git-FirstPush {
     git add .
